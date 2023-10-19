e786cea5d900a6608aa3e7ccc47b57ddb13cae1c
SophianLithoson <140106963+SophianLithoson@users.noreply.github.com>
2023-10-18 19:49:18 -0600

    started making classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log("testing it's working");
+
+
+ class Task {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+ }
+
+ class Project {
+    
+ }
